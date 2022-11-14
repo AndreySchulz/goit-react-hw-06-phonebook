86172a6fcf02ad9cade1888ec45449703d5ef079
Andrey_Schulz <51944693+AndreySchulz@users.noreply.github.com>
2022-11-14 17:08:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,44 +3,7 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
 export function App() {
-  // // const [contacts, setContacts] = useState(
-  // //   JSON.parse(localStorage.getItem('contacts')) ?? []
-  // // );
-  // // const [filter, setFilter] = useState('');
-
-  // // useEffect(() => {
-  // //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // // }, [contacts]);
-  // const contacts = useSelector(getAllContacts);
-  // const dispatch = useDispatch();
-  // const inputValueForm = e => {
-  //   const { value } = e.target;
-
-  //   setFilter(value);
-  // };
-
-  // const addContact = ({ name, number }) => {
-  //   // if (
-  //   //   contacts.some(contact => {
-  //   //     return contact.name === name || contact.number === number;
-  //   //   })
-  //   // ) {
-  //   //   return alert(`${name} is already in contacts`);
-  //   // }
-  //   dispatch(add({ name, number }));
-  // };
-
-  // // const filterContact = () => {
-  // //   if (filter.length === 0) return contacts;
-  // //   const filttredContacts = contacts.filter(el =>
-  // //     el.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-  // // //   );
-  // // //   return filttredContacts;
-  // // };
-  // const removeContact = id => {
-  //   // setContacts(prev => prev.filter(el => el.id !== id));
-  //   dispatch(remove(id));
-  // };
+  
 
   return (
     <>
